23cfe28875d8a969963252d6fc5efd9604423388
Confidence Emonena <confidinho@yahoo.com>
2024-01-02 17:28:01 +0100

    register page

--- src/app/forgot-password/page.jsx
+++ src/app/forgot-password/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Bg from "../../../public/bg.png";
 import Google from "../../../public/google.png";
+import Logo from "../../../public/logo.png";
 import Image from "next/image";
-import { ArrowBack } from "@mui/icons-material";
 import { Box, Checkbox, TextField } from "@mui/material";
 import Link from "next/link";
 
@@ -10,10 +10,9 @@ function ForgotPassword() {
   return (
     <div className="flex flex-row justify-between w-full my-5">
       <div className="bg-white w-[100%] px-[4%] md:px-[8%] md:w-[50%]">
-        <div className="flex flex-row items-center justify-start gap-3">
-          <ArrowBack className="text-[#A3AED0] text-[14px]" />
-          <p className="text-[#A3AED0] text-[14px]">Back to Homepage</p>
-        </div>
+        <Link href="/">
+          <Image src={Logo} alt="logo" className="h-20 object-contain" />
+        </Link>
         <div className=" mt-20 md:20">
           <h2 className="text-[#1E2B3B] text-[18px] md:text-[24px] font-bold">
             Please enter your email address
