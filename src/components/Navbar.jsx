65a3f2d1bf3a3665424ba34d494616e40b5ccb4b
Confidence Emonena <confidinho@yahoo.com>
2024-01-03 10:47:24 +0100

    register page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Logo from "../../public/logo.png";
 import Image from "next/image";
 import { Box, Checkbox, TextField } from "@mui/material";
+import { BiMenuAltRight } from "react-icons/bi";
 import Link from "next/link";
 
 function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
   return (
     <div className="w-full flex flex-row justify-between items-center align-middle py-[1%] px-[6%]">
       <div className="w-full ">
@@ -12,18 +23,41 @@ function Navbar() {
           <Image
             src={Logo}
             alt="logo"
-            className="h-15 w-[10%] flex flex-row  justify-start"
+            className="h-[15%] w-[30%] flex flex-row  justify-start md:h-15 md:w-[10%]"
           />
         </Link>
       </div>
-      <div className="flex flex-row justify-end gap-5 w-[30%]">
+      <button onClick={handleMenuToggle}>
+        <BiMenuAltRight size={30} />
+      </button>
+      {mobileMenuOpen && (
+        <div className="absolute top-12 right-6 mt-2 py-2 w-[200px] bg-white border rounded-lg z-10">
+          <div
+            className={`${
+              mobileMenuOpen ? "flex" : "hidden"
+            } md:hidden flex-col gap-3 h-[120px] p-5  z-10 mt-0`}
+          >
+            <Link href="/login">
+              <button className="border border-[#2B3674] font-bold px-12 p-3 md:px-10 md:text-[16px] text-[12px] rounded-full text-[#2B3674] hover:bg-[#2B3674] hover:text-white">
+                Login
+              </button>
+            </Link>
+            <Link href="/register">
+              <button className=" bg-[#2B3674] font-bold px-12 p-3 md:px-10 md:text-[16px] text-[12px] rounded-full text-white">
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        </div>
+      )}
+      <div className="md:flex flex-row justify-end gap-5 w-[100%] md:w-[35%] hidden">
         <Link href="/login">
-          <button className="border border-[#2B3674] font-bold px-10 p-3 rounded-full text-[#2B3674] hover:bg-[#2B3674] hover:text-white">
+          <button className="border border-[#2B3674] font-bold px-7 p-3 md:px-10 md:text-[16px] text-[12px] rounded-full text-[#2B3674] hover:bg-[#2B3674] hover:text-white">
             Login
           </button>
         </Link>
         <Link href="/register">
-          <button className=" bg-[#2B3674] font-bold px-10 p-3 rounded-full text-white">
+          <button className=" bg-[#2B3674] font-bold px-7 p-3 md:px-10 md:text-[16px] text-[12px] rounded-full text-white">
             Sign Up
           </button>
         </Link>
