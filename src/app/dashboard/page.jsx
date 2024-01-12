b20eb841b1c53414efd492491ef83422c1f24f9f
Confidence Emonena <confidinho@yahoo.com>
2024-01-12 02:32:51 +0100

    pay tsa

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,25 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function Dashboard() {
+  const session = useSession();
+  const router = useRouter();
+
+  if (session.status === "loading") {
+    return (
+      <div className="flex flex-row flex-1 justify-center h-screen">
+        <Image src="/loader.svg" alt="loading" width={100} height={100} />
+      </div>
+    );
+  }
+
+  if (session.status === "unauthenticated") {
+    router?.push("/login");
+  }
   return (
     <div>
       <Navbar />
